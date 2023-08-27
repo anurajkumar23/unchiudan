6f2eb79fa092b48988e9e56685948e361ab2f535
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-28 03:39:42 +0530

    updated

--- frontend/src/Components/Home/HomeUI/Navbar.jsx
+++ frontend/src/Components/Home/HomeUI/Navbar.jsx
@@ -1,69 +1,139 @@
 import React, { useState } from "react";
+import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
 
-function Navbar() {
+const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
   };
 
   return (
-    <div className="w-full z-40 mx-auto flex items-center justify-between">
-      {/* Logo */}
-      <div className="flex items-center">
-        <a
-          href="#"
-          className="flex items-center text-white text-lg font-semibold mr-6"
-        >
-          <img src="/logo.png" alt="Logo" className="h-8 w-8 mr-2" />
-        </a>
-      </div>
-      {/* Links */}
-      <div className="flex items-center">
-        <a href="#" className="text-white text-lg font-semibold mr-6">
-          Home
-        </a>
-        <div className="relative group">
-          <button
+    <nav className="fixed z-40 w-full ">
+      <div className="backdrop-blur mx-auto px-6 md:px-12 lg:px-7">
+        <div className="flex flex-wrap items-center justify-between  gap-6  md:gap-0 relative">
+          <input
+            type="checkbox"
+            name="toggle_nav"
+            id="toggle_nav"
+            className="peer hidden"
+            checked={isMenuOpen}
+            onChange={toggleMenu}
+          />
+          <div className="w-full flex justify-between md:w-max md:px-0">
+            <a href="#" aria-label="logo">
+              <img
+                src="logo.png"
+                className="w-36"
+                alt="logo"
+                width="144"
+                height="68"
+              />
+            </a>
+            <div className="flex items-center md:hidden max-h-10">
+              <label
+                role="button"
+                htmlFor="toggle_nav"
+                aria-label="humburger"
+                id="hamburger"
+                className="relative z-40 px-2 py-3 sm:-mr-6"
+              >
+                {isMenuOpen ? (
+                  <RiCloseFill className="text-black w-7 h-7" />
+                ) : (
+                  <RiMenu3Fill className="text-black w-7 h-7" />
+                )}
+              </label>
+            </div>
+          </div>
+          <label
+            role="button"
+            htmlFor="toggle_nav"
+            className={`fixed w-full z-30 h-full top-0 left-0 bg-gray-700 bg-opacity-40 ${
+              isMenuOpen ? "block" : "hidden"
+            }`}
             onClick={toggleMenu}
-            className="text-white group-hover:text-gray-200 focus:outline-none"
+          ></label>
+          <div
+            className={`flex z-50 flex-col md:flex-row justify-between items-center gap-y-4 p-6 bg-white  md:w-8/12 md:gap-y-4 md:p-0 md:bg-transparent lg:w-7/12 fixed top-0 ${
+              isMenuOpen ? "left-0" : "-left-full"
+            } transition-all duration-500 max-w-sm h-full md:left-0 md:h-auto w-4/5 md:max-w-none md:relative bg-white lg:first-letter:top-0`}
           >
-            Current Affairs
-          </button>
-          {isMenuOpen && (
-            <div className="absolute mt-2 bg-white text-gray-700 py-2 px-4 rounded shadow-md">
-              <a href="#" className="block py-1">
-                SSC
-              </a>
-              <a href="#" className="block py-1">
-                JEE Main
-              </a>
-              <a href="#" className="block py-1">
-                JEE Advanced
+            <div className="flex md:hidden w-full pb-5">
+              <a href="#" aria-label="logo">
+                <img
+                  src="images/logo.svg"
+                  className="w-36 grayscale contrast-200"
+                  alt="logo"
+                  width="144"
+                  height="68"
+                />
               </a>
             </div>
-          )}
+            <div className="block w-full h-full md:h-auto">
+              <ul className="space-y-8 tracking-wide font-medium md:flex md:space-y-0">
+                <li>
+                  <a href="#" className="block md:px-3">
+                    <div
+                      className="relative text-yellow-800 dark:text-yellow-300
+                                                    before:absolute before:-inset-2 before:w-full before:h-0.5 before:origin-left dark:before:bg-yelloe-500 before:mx-auto before:mt-auto before:rounded-full before:bg-yellow-800"
+                    >
+                      <span>Home</span>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="block md:px-3 group">
+                    <div
+                      className="relative text-gray-600
+                                                    before:absolute before:-inset-2 before:w-full before:h-0.5 before:origin-left dark:before:bg-yelloe-500 before:mx-auto before:mt-auto before:rounded-full before:bg-yellow-800 before:transition before:scale-x-0 group-hover:before:scale-x-100"
+                    >
+                      <span className="transition group-hover:text-yellow-700 dark:text-gray-300 dark:group-hover:text-yellow-300">
+                        Current affairs
+                      </span>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="block md:px-3 group">
+                    <div
+                      className="relative text-gray-600
+                                                    before:absolute before:-inset-2 before:w-full before:h-0.5 before:origin-left dark:before:bg-yelloe-500 before:mx-auto before:mt-auto before:rounded-full before:bg-yellow-800 before:transition before:scale-x-0 group-hover:before:scale-x-100"
+                    >
+                      <span className="transition group-hover:text-yellow-700 dark:text-gray-300 dark:group-hover:text-yellow-300">
+                        UPSC
+                      </span>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="w-full gap-y-4 md:w-max md:gap-y-0 md:gap-x-4 flex md:flex-row flex-col">
+              {/* Buttons */}
+              <button
+                type="button"
+                title="Login"
+                className="group w-full py-3 px-6 text-center transition dark:active:bg-yellow-800 dark:focus:bg-yellow-900 active:bg-yellow-200 focus:bg-yellow-100 sm:w-max"
+              >
+                <span className="block text-gray-700 dark:text-gray-300 font-semibold group-focus:text-yellow-700 dark:group-focus:text-yellow-100">
+                  Login
+                </span>
+              </button>
+              <button
+                type="button"
+                title="Shop now"
+                className="w-full rounded-full py-1 px-5 text-center bg-blue-400 md:w-max"
+              >
+                <span className="block text-white font-semibold">
+                  Premium Course
+                </span>
+              </button>
+            </div>
+          </div>
         </div>
-        <a href="#" className="text-white text-lg font-semibold mr-6">
-          UPSC
-        </a>
-        <a href="#" className="text-white text-lg font-semibold mr-6">
-          Downloads
-        </a>
       </div>
-      {/* Search bar */}
-      <div className="flex items-center">
-        <input
-          type="text"
-          placeholder="Search"
-          className="px-4 py-2 rounded-lg border focus:outline-none"
-        />
-        <button className="ml-2 bg-white text-blue-500 px-4 py-2 rounded-lg">
-          Search
-        </button>
-      </div>
-    </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
