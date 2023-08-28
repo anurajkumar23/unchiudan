895716a3c93607cb1e9711333b48999ba5f00305
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-29 00:30:00 +0530

    updated

--- frontend/src/Components/Blogs/Blogs.jsx
+++ frontend/src/Components/Blogs/Blogs.jsx
@@ -14,16 +14,18 @@ export default function Blogs() {
 
   return (
     <div>
-      {blogs.map((blog, index) => (
-        <BlogComp
-          key={index}
-          date={blog.date}
-          title={blog.title}
-          views={blog.views}
-          likes={blog.likes}
-          imageSrc={blog.imageSrc}
-        />
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {blogs.map((blog, index) => (
+          <BlogComp
+            key={index}
+            date={blog.date}
+            title={blog.title}
+            views={blog.views}
+            likes={blog.likes}
+            imageSrc={blog.imageSrc}
+          />
+        ))}
+      </div>
     </div>
   );
 }
