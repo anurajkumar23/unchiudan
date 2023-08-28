09882b8e3028442100d5ca485af4378f68819358
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2023-08-29 02:16:29 +0530

    adding server codes

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ process.on('unhandledRejection', (err) => {
 
 process.on('SIGTERM', () => {
   console.log('👋 SIGTERM RECEIVED. Shutting down gracefully...');
-  server.close(() => {
-    console.log('💥 Process terminated!');
+  server.close(() => { 
+    console.log('💥  Process terminated!');
   });
 });
