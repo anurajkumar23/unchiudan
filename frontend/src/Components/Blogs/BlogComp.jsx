0237df8fccc72b8607d17ee98c513853ca4b99a1
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-29 00:09:37 +0530

    updated

--- frontend/src/Components/Blogs/BlogComp.jsx
+++ frontend/src/Components/Blogs/BlogComp.jsx
@@ -1,39 +1,61 @@
 import React from "react";
 import { FaEye, FaThumbsUp } from "react-icons/fa";
 
-function BlogComp() {
+function BlogComp({ date, title, views, likes }) {
   return (
-    <div>
-      <div class="bg-white p-6 w-[10rem] rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
-        <div class="relative">
-          <img class="w-full rounded-xl" src="/uchiudan.png" alt="Colors" />
-          <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
-            29 August
-          </p>
-        </div>
-        <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">
-          UPSC Training Meow Meow Meow
-        </h1>
-        <div class="my-4 flex justify-between">
-          <div class="flex space-x-1 items-center">
-            <span>
-              <FaEye />
-            </span>
-            <p>125</p>
-          </div>
-          <div class="flex space-x-1 items-center">
-            <span>
-              <FaThumbsUp />
-            </span>
-            <p>36</p>
-          </div>
-        </div>
-        <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
-          Read More
-        </button>
+    <div className="bg-white p-6 w-[10rem] rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+      <div className="relative">
+        <img className="w-full rounded-xl" src="/uchiudan.png" alt="Colors" />
+        <p className="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
+          {date}
+        </p>
       </div>
+      <h1 className="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">
+        {title}
+      </h1>
+      <div className="my-4 flex justify-between">
+        <div className="flex space-x-1 items-center">
+          <span>
+            <FaEye />
+          </span>
+          <p>{views}</p>
+        </div>
+        <div className="flex space-x-1 items-center">
+          <span>
+            <FaThumbsUp />
+          </span>
+          <p>{likes}</p>
+        </div>
+      </div>
+      <button className="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
+        Read More
+      </button>
     </div>
   );
 }
 
-export default BlogComp;
+export default function BlogList() {
+  const blogs = [
+    {
+      date: "29 August",
+      title: "UPSC Training Meow Meow Meow",
+      views: 125,
+      likes: 36,
+    },
+    // Add more blog objects here
+  ];
+
+  return (
+    <div>
+      {blogs.map((blog, index) => (
+        <BlogComp
+          key={index}
+          date={blog.date}
+          title={blog.title}
+          views={blog.views}
+          likes={blog.likes}
+        />
+      ))}
+    </div>
+  );
+}
