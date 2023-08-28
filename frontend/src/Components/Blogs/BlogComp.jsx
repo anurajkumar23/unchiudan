dc9ab55f106e16a828dc31933e12e75a71e3be05
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-29 00:06:10 +0530

    updated

--- frontend/src/Components/Blogs/BlogComp.jsx
+++ frontend/src/Components/Blogs/BlogComp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaEye } from "react-icons/fa";
+import { FaEye, FaThumbsUp } from "react-icons/fa";
 
 function BlogComp() {
   return (
@@ -12,9 +12,9 @@ function BlogComp() {
           </p>
         </div>
         <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">
-          Javascript Bootcamp for Absolute Beginners
+          UPSC Training Meow Meow Meow
         </h1>
-        <div class="my-4">
+        <div class="my-4 flex justify-between">
           <div class="flex space-x-1 items-center">
             <span>
               <FaEye />
@@ -23,46 +23,14 @@ function BlogComp() {
           </div>
           <div class="flex space-x-1 items-center">
             <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 text-indigo-600 mb-1.5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                />
-              </svg>
+              <FaThumbsUp />
             </span>
-            <p>3 Parts</p>
+            <p>36</p>
           </div>
-          <div class="flex space-x-1 items-center">
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 text-indigo-600 mb-1.5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-                />
-              </svg>
-            </span>
-            <p>Vanilla JS</p>
-          </div>
-          <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
-            Buy Lesson
-          </button>
         </div>
+        <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
+          Buy Lesson
+        </button>
       </div>
     </div>
   );
