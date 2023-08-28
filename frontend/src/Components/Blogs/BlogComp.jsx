1eb45761ab804e7c6378398b6ced0f963f52619b
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-29 00:35:44 +0530

    updated

--- frontend/src/Components/Blogs/BlogComp.jsx
+++ frontend/src/Components/Blogs/BlogComp.jsx
@@ -27,7 +27,7 @@ function BlogComp({ date, title, views, likes, imageSrc }) {
           <p>{likes}</p>
         </div>
       </div>
-      <button className="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
+      <button className="mt-4 text-lg w-fit text-white bg-indigo-600 py-2 px-5 rounded-xl shadow-xl">
         Read More
       </button>
     </div>
