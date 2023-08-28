353b1fa7c66877c578c433649af9468701212c09
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-28 23:50:22 +0530

    updated

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./HomeUI/Hero";
 import ServicesOverview from "./HomeUI/ServicesOverview";
+import Blogs from "../Blogs/Blogs";
 function Home() {
   return (
     <div className="mx-auto py-10 space-y-10">
       <Hero />
       <ServicesOverview />
+      <Blogs />
     </div>
   );
 }
