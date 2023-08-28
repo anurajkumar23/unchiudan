18e7eab822eed3ea4078f5df6bba549fe318d58c
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-29 00:34:27 +0530

    updated

--- frontend/src/Components/Blogs/BlogComp.jsx
+++ frontend/src/Components/Blogs/BlogComp.jsx
@@ -3,17 +3,17 @@ import { FaEye, FaThumbsUp } from "react-icons/fa";
 
 function BlogComp({ date, title, views, likes, imageSrc }) {
   return (
-    <div className="bg-white p-6 w-[10rem] rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+    <div className="bg-white p-6 w-[20rem] rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
       <div className="relative">
         <img className="w-full rounded-xl" src={imageSrc} alt="Blog Cover" />
         <p className="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
           {date}
         </p>
       </div>
-      <h1 className="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">
+      <h1 className="mt-4 text-gray-800 text-xl font-bold cursor-pointer">
         {title}
       </h1>
-      <div className="my-4 flex justify-between">
+      <div className="my-4 mx-10 flex justify-between">
         <div className="flex space-x-1 items-center">
           <span>
             <FaEye />
