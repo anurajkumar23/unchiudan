b0c6e5443dc7382fc14080e1de9f1ca1e5f12739
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2023-08-30 02:27:52 +0530

    implemnting protection + adding user middleware

--- backend/models/userModal.js
+++ backend/models/userModal.js
@@ -1,21 +1,64 @@
+const mongoose = require('mongoose');
+const { randomBytes, scrypt: _scrypt } = require('crypto');
+const { promisify } = require('util');
+const scrypt = promisify(_scrypt);
+
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: [true, 'Please tell us your name!'] },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true
-    },
-    role: {
-      type: String,
-      enum: ['user', 'admin'],
-      default: 'user'
-    },
-    password: {
-      type: String,
-      required: [true, 'Please provide a password'],
-      minlength: 8,
-      select: false
-    },
-   
-  });
+  name: { type: String, required: [true, 'Please tell us your name!'] },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordChangedAt: Date,
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = randomBytes(8).toString('hex');
+  const hash = await scrypt(this.password, salt, 32);
+  const result = salt + '.' + hash.toString('hex');
+  this.password = result;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  const [salt, storedHash] = userPassword.split('.');
+  const hash = await scrypt(candidatePassword, salt, 32);
+  if (storedHash !== hash.toString('hex')) {
+    return false;
+  }
+  return true;
+};
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return JWTTimestamp < changedTimestamp;
+  }
+  // False means NOT changed
+  return false;
+};
+
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
