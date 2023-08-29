efa9265f88f25443dba77faeb3f13b62fc8f9017
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-29 17:12:29 +0530

    updated

--- frontend/src/Components/Downloads/PdfComp.jsx
+++ frontend/src/Components/Downloads/PdfComp.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { FaFileAlt } from "react-icons/fa";
 
-function DownloadPDF() {
+function PdfComp() {
   return (
     <div>
       <div className="w-26 flex justify between">
         <div>
           <FaFileAlt className="w-12 h-12" />
         </div>
-        <div></div>
+        <div className="flex flex-col justify-center">
+          <h1 className="text-center text-md"> UPSC Exam PDF Notes 1 </h1>
+          <p>29 August</p>
+        </div>
       </div>
     </div>
   );
 }
 
-export default DownloadPDF;
+export default PdfComp;
