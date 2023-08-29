9c5b90cf92c772720be71a3b1da2d089605cc776
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-08-29 20:43:02 +0530

    add HeroContent

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -3,10 +3,12 @@ import Hero from "./HomeUI/Hero";
 import ServicesOverview from "./HomeUI/ServicesOverview";
 import Blogs from "../Blogs/Blogs";
 import PdfList from "../Downloads/PdfList";
+import HeroContent from "./HomeUI/HeroContent";
 function Home() {
   return (
     <div className="mx-auto py-10 space-y-[8rem]">
       <Hero />
+      <HeroContent/>
       <ServicesOverview />
       <Blogs />
       <PdfList />
