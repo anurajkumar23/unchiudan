4f67e018d0ee80d5fb63aea450f507620bd959e1
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-29 15:35:15 +0530

    updated

--- frontend/src/Components/Blogs/Blogs.jsx
+++ frontend/src/Components/Blogs/Blogs.jsx
@@ -31,6 +31,11 @@ export default function Blogs() {
       <h1 className="text-center text-1.8rem md:text-[2rem] mb-6 ">
         Latest Current Affairs
       </h1>
+      <p className="text-justify text-md">
+        Current Affairs for BPSC, UPPSC, MPPSC, JPSC, BSSC, RPSC, SSC, और अन्य
+        Competitive और&nbsp;Government Job Examinations के लिए ऊँची उड़ान वेबसाइट
+        और फेसबुक पेज को Follow करें।{" "}
+      </p>
       <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-6">
         {blogs.map((blog, index) => (
           <BlogComp
