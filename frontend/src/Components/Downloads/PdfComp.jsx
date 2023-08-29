f2d55a5e0797c931dccb33a453e81de24d67b642
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-29 17:23:09 +0530

    updated

--- frontend/src/Components/Downloads/PdfComp.jsx
+++ frontend/src/Components/Downloads/PdfComp.jsx
@@ -3,7 +3,7 @@ import { FaFileAlt } from "react-icons/fa";
 
 function PdfComp({ title, date }) {
   return (
-    <div className="w-26 flex justify-between p-4 border border-2 rounded-lg">
+    <div className="w-24 flex justify-between p-4 border border-2 rounded-lg">
       <div>
         <FaFileAlt className="w-12 h-12" />
       </div>
