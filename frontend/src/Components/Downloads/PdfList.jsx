ecf553b36b5b1a45b9c42ffa72f6fb405fbb5a1b
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-29 17:17:45 +0530

    updated

--- frontend/src/Components/Downloads/PdfList.jsx
+++ frontend/src/Components/Downloads/PdfList.jsx
@@ -10,10 +10,20 @@ export default function PdfList() {
   ];
 
   return (
-    <div>
-      {pdfs.map((pdf, index) => (
-        <PdfComp key={index} title={pdf.title} date={pdf.date} />
-      ))}
+    <div className="mx-10">
+      <h1 className="text-center text-[1.5rem] md:text-[2rem] mb-4 ">
+        Latest Current Affairs
+      </h1>
+      <p className="text-justify text-md mb-16 md:mx-[6rem]">
+        Current Affairs for BPSC, UPPSC, MPPSC, JPSC, BSSC, RPSC, SSC, और अन्य
+        Competitive और&nbsp;Government Job Examinations के लिए ऊँची उड़ान वेबसाइट
+        और फेसबुक पेज को Follow करें।
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-6">
+        {pdfs.map((pdf, index) => (
+          <PdfComp key={index} title={pdf.title} date={pdf.date} />
+        ))}
+      </div>
     </div>
   );
 }
