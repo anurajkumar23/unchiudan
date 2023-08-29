50c03bf686a07bf0b5ce07a2f8dfe38b24c252be
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-29 17:14:30 +0530

    updated

--- frontend/src/Components/Downloads/PdfComp.jsx
+++ frontend/src/Components/Downloads/PdfComp.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { FaFileAlt } from "react-icons/fa";
 
-function PdfComp() {
+function PdfCard({ title, date }) {
   return (
-    <div>
-      <div className="w-26 flex justify between">
-        <div>
-          <FaFileAlt className="w-12 h-12" />
-        </div>
-        <div className="flex flex-col justify-center">
-          <h1 className="text-center text-md"> UPSC Exam PDF Notes 1 </h1>
-          <p>29 August</p>
-        </div>
+    <div className="w-26 flex justify-between">
+      <div>
+        <FaFileAlt className="w-12 h-12" />
+      </div>
+      <div className="flex flex-col justify-center">
+        <h1 className="text-center text-md">{title}</h1>
+        <p>{date}</p>
       </div>
     </div>
   );
 }
 
-export default PdfComp;
+export default PdfCard;
