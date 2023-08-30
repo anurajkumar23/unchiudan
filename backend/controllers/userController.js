7ccc28466f00c6363fbf24feb86db111a4a11620
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2023-08-30 09:14:24 +0530

    implemnting protection + adding user middleware (#10)

---
+++ backend/controllers/userController.js
@@ -0,0 +1,13 @@
+const User = require('../models/userModal');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
--- backend/router/userRoutes.js
+++ backend/router/userRoutes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const userController = require('../controllers/userController.js');
+const authController = require('../controllers/authController.js');
 
 const router = express.Router();
 
-// router.post('/login', userController.login);
-// router.get('/logout', userController.logout);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+
+
+router
+  .route('/')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.getAllUsers,
+  );
 
 module.exports = router;
