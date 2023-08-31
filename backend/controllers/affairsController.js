da63992475b913691f6b67bdbeb9f2f8596a5d28
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2023-09-01 00:40:44 +0530

    Affairs Completed 😃😃 (#24)
    * implemnting protection + adding user middleware
    
    * completed news api
    
    * deleting unwated images
    
    * completed user api
    
    * adding pagination to news
    
    * resolve conflict
    
    * pagination2
    
    * adding auto delete
    
    * add restriction in auto delete
    
    * slove some bug
    
    * make multer dynamic
    
    * fix some bugs and implement basic func of affairs
    
    * Complete section  Affairs with advanced searches

--- backend/controllers/affairsController.js
+++ backend/controllers/affairsController.js
@@ -3,20 +3,38 @@ const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getAllAffairs = catchAsync(async (req, res, next) => {
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 10;
-  const skip = (page - 1) * limit;
-  // Create a query to retrieve news articles with pagination
-  const query = CurrentAffairs.find().skip(skip).limit(limit);
-  const affairs = await query;
-  res.status(200).json({
-    status: 'success',
-    results: affairs.length,
-    data: {
-      affairs,
-    },
+    // Build query
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'limit', 'sort', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+  
+    if (queryObj.category) {
+      queryObj.category = {
+        $regex: new RegExp(queryObj.category, 'i'), 
+      };
+    }
+  
+    let query = CurrentAffairs.find(queryObj).sort('-createdAt');
+  
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+  
+    // Execute query
+    const affairs = await query;
+  
+    res.status(200).json({
+      status: 'success',
+      results: affairs.length,
+      data: {
+        affairs,
+      },
+    });
   });
-});
+  
+  
 
 exports.createAffairs = catchAsync(async (req, res, next) => {
   const affairs = await CurrentAffairs.create(req.body);
@@ -58,13 +76,16 @@ exports.deleteAffair = catchAsync(async (req, res, next) => {
 });
 
 exports.updateOne = catchAsync(async (req, res, next) => {
-  const affairs = await CurrentAffairs.findByIdAndUpdate(req.params.id, req.body,{
+  const affairs = await CurrentAffairs.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
       new: true,
       runValidators: true,
     },
   );
-//   console.log(affairs);
- 
+  //   console.log(affairs);
+
   console.log(req.body);
   if (!affairs) {
     return next(new AppError('No doc found with that ID', 404));
