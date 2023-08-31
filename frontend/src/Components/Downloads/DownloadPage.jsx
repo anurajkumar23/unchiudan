ab4db28b2b8bab4ec4d7d31df0e2784616917026
Kingshuk Mondal <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-31 20:30:01 +0530

    Everything Fully Responsive,, Homepage, Blogs, Downloads, Current affairs etc,, all complete & fully responsive... (#22)
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated

--- frontend/src/Components/Downloads/DownloadPage.jsx
+++ frontend/src/Components/Downloads/DownloadPage.jsx
@@ -12,8 +12,8 @@ import {
 function DownloadPage() {
   return (
     <div className="mx-auto py-[8rem] ">
-      <div className="flex ">
-        <div className="w-2/3 p-4 overflow-y-auto">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
+        <div className="mx-0 col-span-2 xl:mx-0 p-4 md:mx-0 overflow-y-auto lg:my-0">
           <h1 className="text-center font-bold text-[2rem] md:text-[2.5rem] mb-6 ">
             UPSC download title 1 pdf note 1
           </h1>
@@ -25,15 +25,15 @@ function DownloadPage() {
             />
           </div>
 
-          <div className="w-18 mx-12 flex-col p-4 border border-2 rounded-lg mt-16">
-            <div className="flex justify-between ">
+          <div className="w-18 md:mx-12 flex-col p-4 border border-2 mx-4 rounded-lg mt-16">
+            <div className="flex justify-between space-x-3 ">
               <FaFileAlt className="w-12 h-12" />
 
               <h1 className="text-center text-lg ">
                 Monthly Current Affairs of Jun 2020 PDF Download Set no- 205
               </h1>
             </div>
-            <div className="flex justify-between mx-12  mt-4">
+            <div className="flex justify-between md:mx-12  mt-4">
               <span>Size: 10MB</span> <span>Downloads: 125</span>
               <span>Last Updated: 30 August</span>
             </div>
@@ -44,9 +44,9 @@ function DownloadPage() {
               </div>
             </a>
           </div>
-          <div className="flex justify-between mt-6 mx-12 ">
-            <span className="text-center text-lg ">Share with Friends :</span>
-            <span className="flex text-gray-400 justify-center space-x-5">
+          <div className="flex justify-between mt-6  ">
+            <span className="text-center text-md ">Share with Friends :</span>
+            <span className="flex text-gray-400 justify-center space-x-4">
               <a className=" " href="" target="_blank" rel="noreferrer">
                 <FaFacebook className="text-blue-500 w-7 h-7" />
               </a>
@@ -82,9 +82,9 @@ function DownloadPage() {
             by Unchi Udaan. You can also Download the Previous Month PDF for
             Free Monthly PDF. You can also download other PDFs{" "}
           </p>
-          <div className="flex justify-between mt-10 mx-12 ">
-            <span className="text-center text-lg ">Share with Friends :</span>
-            <span className="flex text-gray-400 justify-center space-x-5">
+          <div className="flex justify-between mt-10 ">
+            <span className="text-center text-md ">Share with Friends :</span>
+            <span className="flex text-gray-400 justify-center space-x-4">
               <a className=" " href="" target="_blank" rel="noreferrer">
                 <FaFacebook className="text-blue-500 w-7 h-7" />
               </a>
@@ -107,14 +107,14 @@ function DownloadPage() {
           </div>
         </div>
 
-        <div className="w-1/3 p-4 space-y-10">
-          <div className="flex items-center">
+        <div className=" p-4 space-y-10">
+          <div className="flex md:mx-0 items-center mx-4">
             <input
               type="text"
               placeholder="Search..."
               className="px-4 py-2 w-full rounded-md border border-gray-500 focus:outline-none focus:border-indigo-500"
             />
-            <button className="absolute right-4 bg-indigo-500 text-white p-3 rounded-md flex items-center hover:bg-indigo-600 focus:outline-none">
+            <button className="absolute right-10  bg-indigo-500 text-white p-3 rounded-md  flex items-center md:right-4 hover:bg-indigo-600 focus:outline-none">
               <FaSearch className="mx-2" />
             </button>
           </div>
