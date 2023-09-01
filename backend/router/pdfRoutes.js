641d5867f81158082a9037447ac2822695b97088
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2023-09-01 14:36:58 +0530

    adding basic func of pdf

--- backend/router/pdfRoutes.js
+++ backend/router/pdfRoutes.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const pdfController = require('../controllers/pdfContoller');
+const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
+
+router.route('/').get(pdfController.getAllPdf).post(
+  protect,
+  // restrictTo("admin"),
+  pdfController.uploadPhotoAndPdf,
+  pdfController.createPdf,
+);
+
+router
+  .route('/:id')
+  .get(pdfController.getPdf)
+  .delete(
+    protect,
+    // restrictTo('admin'),
+    pdfController.deletePdf,
+  )
+  .patch(
+    protect,
+    restrictTo('admin'),
+    pdfController.uploadPhotoAndPdf,
+    pdfController.updateOne,
+  );
 
 module.exports = router;
