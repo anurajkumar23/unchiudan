ae3643b5ddd863b3c2a5b22312d92fb27a956745
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-09-09 15:59:34 +0530

    updated

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Hero from "./HomeUI/Hero";
 import ServicesOverview from "./HomeUI/ServicesOverview";
-import Blogs from "../Blogs/Blogs";
+import HomeBlogs from "../Blogs/HomeBlogs";
 import PdfList from "../Downloads/PdfList";
-
 
 function Home() {
   return (
     <div className="mx-auto py-10 space-y-[8rem]">
       <Hero />
       <ServicesOverview />
-      <Blogs />
+      <HomeBlogs />
       <PdfList />
-
     </div>
   );
 }
