cb3834f092dea2c2766a31c62719c35e901e2513
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-09-09 16:19:38 +0530

    updated

--- frontend/src/Components/Downloads/Downloads.jsx
+++ frontend/src/Components/Downloads/Downloads.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
-import BlogComp from "../Blogs/BlogComp";
+
+import { FaEye, FaThumbsUp } from "react-icons/fa";
+
 function Downloads() {
   const blogs = [
     {
@@ -24,15 +26,77 @@ function Downloads() {
       likes: 36,
       imageSrc: "/uchiudan.png",
     },
+    {
+      date: "29 August",
+      title: "UPSC Training Meow Meow Meow",
+      views: 125,
+      likes: 36,
+      imageSrc: "/uchiudan.png",
+    },
+    {
+      date: "29 August",
+      title: "UPSC Training Meow Meow Meow",
+      views: 125,
+      likes: 36,
+      imageSrc: "/uchiudan.png",
+    },
+    {
+      date: "29 August",
+      title: "UPSC Training Meow Meow Meow",
+      views: 125,
+      likes: 36,
+      imageSrc: "/uchiudan.png",
+    },
+    {
+      date: "29 August",
+      title: "UPSC Training Meow Meow Meow",
+      views: 125,
+      likes: 36,
+      imageSrc: "/uchiudan.png",
+    },
     // dummy test blogs
   ];
+
+  function BlogComps({ date, title, views, likes, imageSrc }) {
+    return (
+      <div className="bg-white p-6 w-[18rem]  md:w-[14rem] rounded-xl shadow-lg  transition duration-500">
+        <div className="relative">
+          <img className="w-full rounded-xl" src={imageSrc} alt="Blog Cover" />
+          <p className="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
+            {date}
+          </p>
+        </div>
+        <h1 className="mt-4 text-gray-800 text-lg font-bold cursor-pointer">
+          {title}
+        </h1>
+        <div className="my-2 mx-6 flex justify-between">
+          <div className="flex space-x-1 items-center">
+            <span>
+              <FaEye />
+            </span>
+            <p>{views}</p>
+          </div>
+          <div className="flex space-x-1 items-center">
+            <span>
+              <FaThumbsUp />
+            </span>
+            <p>{likes}</p>
+          </div>
+        </div>
+        <button className="mt-4 text-md hover:bg-indigo-600 w-full text-white bg-indigo-400 py-1 px-3 rounded-xl hover:shadow-xl">
+          Read More
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="mx-auto py-[8rem] ">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
         <div className="mx-0 col-span-2 xl:mx-0 p-4 md:mx-0 overflow-y-auto lg:my-0">
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 mx-10 md:mx-0 gap-6">
             {blogs.map((blog, index) => (
-              <BlogComp
+              <BlogComps
                 key={index}
                 date={blog.date}
                 title={blog.title}
