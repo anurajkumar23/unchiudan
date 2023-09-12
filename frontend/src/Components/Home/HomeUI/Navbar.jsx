c7021b5f1442afb651d1c754e05c8446084fa554
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-09-12 12:37:12 +0530

    fixed bugs of navbar, added current affairs

--- frontend/src/Components/Home/HomeUI/Navbar.jsx
+++ frontend/src/Components/Home/HomeUI/Navbar.jsx
@@ -1,28 +1,34 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
+
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <nav className="backdrop-blur text-black p-2 fixed z-40 w-full">
-      <div className="flex justify-between items-center">
+    <nav className="backdrop-blur text-black p-2 fixed z-50 w-full shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
         <Link to="/">
           <img src="/uchiudan.png" className="w-12 md:w-20" alt="logo" />
         </Link>
         <div className="md:hidden">
           <button
             onClick={toggleMenu}
-            className="text-white hover:text-gray-300 focus:outline-none"
+            className="text-black hover:text-gray-300 focus:outline-none"
           >
             {isMenuOpen ? (
-              <RiCloseFill className="text-black w-9 h-9" />
+              <RiCloseFill className="text-2xl" />
             ) : (
-              <RiMenu3Fill className="text-black w-7 h-7" />
+              <RiMenu3Fill className="text-2xl" />
             )}
           </button>
         </div>
@@ -30,28 +36,68 @@ const Navbar = () => {
           <Link to="/" className="block">
             Home
           </Link>
-          <Link to="/currentaffairs" className="block">
-            Current Affairs
-          </Link>
-
+          <div className="relative group">
+            <button
+              onClick={toggleDropdown}
+              className="block focus:outline-none"
+            >
+              Current Affairs
+            </button>
+            {isDropdownOpen && (
+              <div className="absolute z-10 mt-2 py-2 w-48 bg-white border rounded-lg shadow-md">
+                <Link to="/currentaffairs" className="block px-4 py-2 hover:bg-blue-100">
+                  UPSC
+                </Link>
+                <Link to="/currentaffairs" className="block px-4 py-2 hover:bg-blue-100">
+                  BPSC
+                </Link>
+                <Link to="/currentaffairs" className="block px-4 py-2 hover:bg-blue-100">
+                  बिहार दारोगा
+                </Link>
+                <Link to="/currentaffairs" className="block px-4 py-2 hover:bg-blue-100">
+                  SSC
+                </Link>
+                <Link to="/currentaffairs" className="block px-4 py-2 hover:bg-blue-100">
+                  रेलवे
+                </Link>
+              </div>
+            )}
+          </div>
           <Link to="/downloads">
-            <span className="w-full rounded-full py-1 px-5  bg-blue-300 md:w-max hover:bg-blue-500 text-white text-center font-semibold ">
+            <span className="w-full rounded-full py-1 px-5  bg-blue-300 md:w-max hover:bg-blue-500 text-white text-center font-semibold shadow-md">
               Premium Course
             </span>
           </Link>
         </div>
       </div>
       {isMenuOpen && (
-        <div className="md:hidden ">
+        <div className="md:hidden">
           <Link to="/" className="block py-2">
             Home
           </Link>
-          <Link to="/currentaffairs" className="block py-2">
-            current affairs
-          </Link>
-
+          <div className="relative group">
+            <button
+              onClick={toggleDropdown}
+              className="block py-2 focus:outline-none"
+            >
+              Current Affairs
+            </button>
+            {isDropdownOpen && (
+              <div className="absolute z-10 mt-2 py-2 w-48 bg-white border rounded-lg shadow-md">
+                <Link to="/currentaffairs" className="block px-4 py-2 hover:bg-blue-100">
+                  Section 1
+                </Link>
+                <Link to="/currentaffairs" className="block px-4 py-2 hover:bg-blue-100">
+                  Section 2
+                </Link>
+                <Link to="/currentaffairs" className="block px-4 py-2 hover:bg-blue-100">
+                  Section 3
+                </Link>
+              </div>
+            )}
+          </div>
           <Link to="/downloads">
-            <span className="w-full rounded-full py-1 px-5  bg-blue-300 md:w-max hover:bg-blue-500 text-white text-center font-semibold ">
+            <span className="w-full rounded-full py-1 px-5  bg-blue-300 md:w-max hover:bg-blue-500 text-white text-center font-semibold shadow-md">
               Premium Course
             </span>
           </Link>
