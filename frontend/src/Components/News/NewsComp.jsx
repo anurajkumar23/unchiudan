4fb5c4ddd7c9a3fd428ed2331d41647e4d5caef3
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-09-17 02:50:28 +0530

    updated

--- frontend/src/Components/News/NewsComp.jsx
+++ frontend/src/Components/News/NewsComp.jsx
@@ -3,7 +3,21 @@ import React from "react";
 function NewsComp() {
   return (
     <div>
-      <h1>hi</h1>
+      <div className="mx-auto w-[21rem]  ">
+        <div className="grid-cols-3">
+          <div className="">
+            <img
+              className="w-full rounded-xl"
+              src={imageSrc}
+              alt="Blog Cover"
+            />
+          </div>
+          <div className="cols-span-2 mx-auto">
+            <h1>Hello Blog 1 </h1>
+            <p>meow meow mwow</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
