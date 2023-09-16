50cc2284bce6fcd8afe988ece571bbab19dec80e
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-09-17 03:22:21 +0530

    updated

--- frontend/src/Components/News/News.jsx
+++ frontend/src/Components/News/News.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import NewsComp from "./NewsComp";
+const newsData = [
+  {
+    category: "UPSC",
+    title: "UPSC Notes 1",
+    content: "meow meow meow",
+    image: "/Images/upsc.jpeg",
+  },
+  // Add more news items here
+];
 function News() {
   return (
-    <div>
-      <h1>Latest News</h1>
-      <NewsComp />
+    <div className="mx-auto">
+      <div className="grid grid-cols-1 gap-6 mx-auto">
+        <NewsComp newsItems={newsData} />
+      </div>
     </div>
   );
 }
