f118b4fefb17de61a0df1dafacb87c77a0c53618
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-09-17 02:52:40 +0530

    updated

---
+++ frontend/src/Components/News/News.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NewsComp from "./NewsComp";
+function News() {
+  return (
+    <div>
+      <h1>Latest News</h1>
+      <NewsComp />
+    </div>
+  );
+}
+
+export default News;
