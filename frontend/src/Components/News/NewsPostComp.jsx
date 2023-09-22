a9ccf60b887cfd59654ad439c707acd0e510723f
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-09-22 20:18:06 +0530

    updated

--- frontend/src/Components/News/NewsPostComp.jsx
+++ frontend/src/Components/News/NewsPostComp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FaImage } from "react-icons/fa";
 
 const CreatePost = ({ onPostSubmit }) => {
   const [title, setTitle] = useState("");
@@ -57,19 +58,19 @@ const CreatePost = ({ onPostSubmit }) => {
 
   return (
     <div className="border p-4 mb-4 mx-auto w-4/5 rounded-lg bg-richblack-5">
-      <h2 className="text-xl font-bold mb-2">Create a New Post</h2>
+      <h2 className="text-xl font-bold mb-2 text-center">Create a New Post</h2>
       <input
         type="text"
         placeholder="Title"
         value={title}
         onChange={handleTitleChange}
-        className="w-full mb-2 p-2 border rounded"
+        className="w-full mb-2 p-2 border rounded-lg"
       />
       <textarea
         placeholder="Content"
         value={content}
         onChange={handleContentChange}
-        className="w-full mb-2 p-2 border rounded"
+        className="w-full mb-2 p-2 border rounded-lg"
         rows="4"
       />
       <input
@@ -77,14 +78,20 @@ const CreatePost = ({ onPostSubmit }) => {
         placeholder="Author"
         value={author}
         onChange={handleAuthorChange}
-        className="w-full mb-2 p-2 border rounded"
+        className="w-full mb-2 p-2 border rounded-lg"
       />
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleImageChange}
-        className="mb-4"
-      />
+      <div className="flex items-center mb-4">
+        <label htmlFor="imageUpload" className="cursor-pointer">
+          <FaImage className="w-8 h-8 text-blue-500" />
+        </label>
+        <input
+          type="file"
+          id="imageUpload"
+          accept="image/*"
+          onChange={handleImageChange}
+          className="hidden"
+        />
+      </div>
       {imagePreview && (
         <div className="mb-4">
           <img src={imagePreview} alt="Preview" className="max-w-full" />
