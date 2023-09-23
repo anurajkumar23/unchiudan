c8ba0321a606e749dc973abd5e5cb3b4387fb0df
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2023-09-23 15:12:40 +0530

    beautify the dashboard

--- frontend/src/Components/Home/core/Auth/UserSettings.jsx
+++ frontend/src/Components/Home/core/Auth/UserSettings.jsx
@@ -1,4 +1,5 @@
 import "./usersetting.css";
+
 function UserSettings() {
   const user = {
     role: "admin",
@@ -9,11 +10,11 @@ function UserSettings() {
       <div className="bg-white max-w-screen-xl mx-auto min-h-screen rounded-3xl overflow-hidden shadow-md flex flex-col sm:flex-row">
         <div className="bg-[#55c57a] sm:w-1/4 p-4">
           <ul className="side-nav">
-            <li className="mb-4 text-white font-bold">Settings</li>
-            <li className="mb-4 text-white font-bold">Study Material</li>
-            <li className="mb-4 text-white font-bold">Billing</li>
+            <li className="mb-4 text-white font-bold pl-[1.3rem] cursor-pointer">Settings</li>
+            <li className="mb-4 text-white font-bold pl-[1.3rem] cursor-pointer">Study Material</li>
+            <li className="mb-4 text-white font-bold pl-[1.3rem] cursor-pointer">Billing</li>
             {user.role === "admin" ? (
-              <li className="mb-4 text-white font-bold">Admin power</li>
+              <li className="mb-4 text-white font-bold pl-[1.3rem] cursor-pointer">Admin power</li>
             ) : (
               ""
             )}
@@ -21,21 +22,21 @@ function UserSettings() {
         </div>
         <div className="sm:w-3/4">
           <div className="user-view__form-container max-w-screen-xl mx-auto px-4 sm:px-8 md:px-16 lg:px-32">
-            <h2 className="heading-secondary mt-8 mb-4 text-2xl text-[#55c57a]">
+            <h2 className="heading-secondary mt-8 mb-4 text-xl text-[#55c57a]">
               Your account settings
             </h2>
             <form className="">
               <div className="mb-4">
                 <label className="block">Name</label>
-                <input className="form__input border border-gray-300 p-2 rounded" />
+                <input className="form__input border border-gray-300 p-2 rounded" placeholder="Name" />
               </div>
               <div className="form__group mb-4">
                 <label className="block">Email address</label>
-                <input className="form__input border border-gray-300 p-2 rounded" />
+                <input className="form__input border border-gray-300 p-2 rounded" placeholder="Email Adress"/>
               </div>
               <div className="form__group mb-4">
                 <label className="block">Phone Number</label>
-                <input className="form__input border border-gray-300 p-2 rounded" />
+                <input className="form__input border border-gray-300 p-2 rounded" placeholder="Number"/>
               </div>
 
               <div className="text-right">
@@ -47,19 +48,19 @@ function UserSettings() {
           </div>
           <hr className="my-8 " />
           <div className="user-view__form-container max-w-screen-xl mx-auto px-4 sm:px-8 md:px-16 lg:px-32">
-            <h2 className="mt-8 mb-4 text-2xl">Password change</h2>
+            <h2 className="mt-8 mb-4 text-xl text-[#55c57a]">Password change</h2>
             <form className="">
               <div className=" mb-4">
-                <label className="block">Old Password</label>
-                <input className="form__input border border-gray-300 p-2 rounded" />
+                <label className="block">Current Password</label>
+                <input className="form__input border border-gray-300 p-2 rounded" placeholder="••••••••"/>
               </div>
               <div className=" mb-4">
                 <label className="block">New Password</label>
-                <input className="form__input border border-gray-300 p-2 rounded" />
+                <input className="form__input border border-gray-300 p-2 rounded" placeholder="••••••••"/>
               </div>
               <div className=" mb-4">
                 <label className="block">Confirm Password</label>
-                <input className="form__input border border-gray-300 p-2 rounded" />
+                <input className="form__input border border-gray-300 p-2 rounded" placeholder="••••••••"/>
               </div>
               <div className="text-right">
                 <button className="bg-[#55c57a] text-white px-4 py-2 rounded mb-[45px]">
