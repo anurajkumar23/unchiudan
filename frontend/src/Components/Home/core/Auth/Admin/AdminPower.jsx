7fbd07758f9ceabb2850c438e39b0f40f4e8cabe
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2023-10-15 16:19:00 +0530

    add delete to admin + change pdf prices

--- frontend/src/Components/Home/core/Auth/Admin/AdminPower.jsx
+++ frontend/src/Components/Home/core/Auth/Admin/AdminPower.jsx
@@ -2,12 +2,52 @@ import CurrentAffairsForm from "./FormCurrentAffairs";
 import FormNews from "./FormNews";
 import SidebarAdmin from "./SidebarAdmin";
 import FormPdf from "./FormPDF";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 
 
 
 
 const AdminPage = ({userData}) => {
+
+  const handleDeleteClick = async (event, newsId) => {
+    console.log("🚀 ~ file: NewsComp.jsx:33 ~ handleDeleteClick ~ newsId:", newsId)
+    event.preventDefault(); // Prevent default behavior (e.g., navigation)
+    event.stopPropagation(); // Prevent the click event from propagating to the parent link element
+
+    if (window.confirm("Are you sure you want to delete this item?")) {
+      const token = localStorage.getItem("jwt_token");
+
+      try {
+        const response = await axios.delete(
+          // `${import.meta.env.VITE_BACKEND_URL}/news/autodelete`,
+          `http://localhost:3000/api/news/autodelete`,
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+        if (response.status === 200) {
+          // The news item was deleted successfully
+          // Show a success toast
+          toast.success("News item deleted successfully");
+
+          // Perform any additional actions you need here
+          console.log("News item deleted successfully");
+        } else {
+          console.error("Error deleting news item:", response);
+          // Show an error toast if needed
+          toast.error("Error deleting news item");
+        }
+      } catch (error) {
+        console.error("Error deleting news item:", error);
+        // Show an error toast if needed
+        toast.error("Error deleting news item");
+      }
+    }
+  };
 
   return (
     <div className="flex  ">
@@ -16,6 +56,9 @@ const AdminPage = ({userData}) => {
       <FormNews />
       <CurrentAffairsForm/>
       <FormPdf/>
+      <div>❗❗ Delete News that are older than 90 Days ❗❗<br/>
+        <button className="bg-[#e10707] text-white rounded p-2" onClick={handleDeleteClick}>Delete News</button>
+      </div>
     </div>
     </div>
   );
