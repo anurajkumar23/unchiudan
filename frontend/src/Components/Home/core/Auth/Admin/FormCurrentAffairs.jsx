bb53746658451c10252ae5586af5a360ba607d96
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-16 00:56:51 +0530

    updated

--- frontend/src/Components/Home/core/Auth/Admin/FormCurrentAffairs.jsx
+++ frontend/src/Components/Home/core/Auth/Admin/FormCurrentAffairs.jsx
@@ -231,7 +231,7 @@ const FormCurrentAffairs = () => {
                 ],
               })
             }
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
           >
             Add Question
           </button>
