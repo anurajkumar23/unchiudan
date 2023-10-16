f4569258addcb141371e2aed082927a0bb910705
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2023-10-16 23:45:59 +0530

    loader applied when data is loaded

--- frontend/src/Components/Downloads/DownloadPage.jsx
+++ frontend/src/Components/Downloads/DownloadPage.jsx
@@ -39,7 +39,16 @@ function DownloadPage({ userData }) {
   }, [id]);
 
   if (!pdfDetails) {
-    return <div>Loading...</div>;
+    return <div className="flex justify-center items-center h-screen">
+    <div
+      className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-info motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status"
+    >
+      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+        Loading...
+      </span>
+    </div>
+  </div>;
   }
 
   const formatUpdatedAtDate = () => {
@@ -64,6 +73,7 @@ function DownloadPage({ userData }) {
     );
     if (pdfDetails.status === "free" || alreadybuy) {
       const downloadLink = `${import.meta.env.VITE_BACKEND_URL}/pdfs/download-pdf/${id}`;
+     
       const anchor = document.createElement("a");
       anchor.href = downloadLink;
       anchor.download = "Unchi_Uddan.pdf";
