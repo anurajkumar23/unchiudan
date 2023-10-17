3937f47eed1c290b7b90f9013b6be647efe461ce
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-10-17 20:22:48 +0530

    updated

--- frontend/src/Components/Home/HomeUI/DevTeam.jsx
+++ frontend/src/Components/Home/HomeUI/DevTeam.jsx
@@ -4,7 +4,7 @@ function DevTeam() {
   return (
     <div className=" space-y-[10rem]">
       <div>
-        <h1 className="text-[3rem] text-center text-white py-[6rem]">
+        <h1 className="text-[3rem] text-center py-[6rem]">
           Developers of <HighlightText text={"ऊँची उड़ान"} />
         </h1>
       </div>
